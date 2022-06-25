1fc7bcc876fbfd73f78eb97c355d5be52dff4599
krtog <tkrstic@gmail.com>
2022-06-25 19:12:53 -0400

    mongodb connected aded ejs, public, urlencoded, json and cors

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 const express = require('express')
-const app = expess()
+const app = express()
 const cors = require('cors')
+const { application } = require('express')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+const PORT = 8000
 
 let db,
     dbConnectionString = process.env.DB_STRING,
-    dbName = '',
+    dbName = 'genetics',
     collection
 
 MongoClient.connect(dbConnectionString)
     .then(client =>{
-        console.log('Connected to Database!')
+        console.log(`Connected to Database!`)
         db = client.db(dbName)
         collection = db.collection('geneticcompanies')
     })
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(cors())
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running on port`)
+})
